3de67c6842cd83d66fd90cd322a75d3273272059
coolinnF <aaa12312312312312@gmail.com>
2026-03-09 01:47:42 -0700

    metals

--- source/main.cpp
+++ source/main.cpp
@@ -3,14 +3,21 @@
 #include "float.h"
 
 #include "../headers/camera.h"
-#include "../headers/hitable.h"
+#include "../headers/materials.h"
 #include "../headers/hitablelist.h"
 #include "../headers/sphere.h"
 
-vec3 color(const ray& r, hitable *world) {
+vec3 color(const ray& r, hitable *world, int depth) {
     hit_record rec;
-    if (world->hit(r, 0.0, FLT_MAX, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if (world->hit(r, 0.001, FLT_MAX, rec)) {
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation*color(scattered, world, depth+1);
+        }
+        else {
+            return vec3(0, 0, 0);
+        }
     }
     else {
         vec3 unit_direction = unit_vector(r.direction());
@@ -21,14 +28,16 @@ vec3 color(const ray& r, hitable *world) {
 
 int main() {
     srand(std::time(NULL));
-    int nx = 500;
-    int ny = 250;
+    int nx = 300;
+    int ny = 150;
     int ns = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    hitable *list[2];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 100);
-    hitable *world = new hitable_list(list, 2);
+    hitable *list[4];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.5));
+    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 1));
+    hitable *world = new hitable_list(list, 4);
     camera cam;
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
@@ -39,9 +48,10 @@ int main() {
 
                 ray r = cam.get_ray(u, v);
                 vec3 p = r.point_at_parameter(2.0);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
             col /= float(ns);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2])); // Less dark of shadow
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
